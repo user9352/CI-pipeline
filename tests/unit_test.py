7fcc607453b6e179f2e6bd018d55f127d5be72dc
Test DP <osima1@student.euba.sk>
2023-04-21 22:30:34 +0200

    Added unit test

---
+++ tests/unit_test.py
@@ -0,0 +1,10 @@
+from src import createProduct, calculateTotalPrice, askForProduct 
+
+def test_createProduct():
+    product = createProduct("Test Product", 5.5)
+    assert product == {"name": "Test Product", "price": 5.5}
+
+def test_calculateTotalPrice():
+    products = [{"name": "Product 1", "price": 6}, {"name": "Product 2", "price": 3.51}]
+    total = calculateTotalPrice(products)
+    assert total == 9.51
