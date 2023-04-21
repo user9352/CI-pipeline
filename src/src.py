a64188745e268b14852254d72a796aff048e4946
Test DP <osima1@student.euba.sk>
2023-04-22 00:15:37 +0200

    b

--- src/src.py
+++ src/src.py
@@ -13,19 +13,19 @@ def calculateTotalPrice(products):
     return totalPrice
 
 def askForProduct():
-    while True:
-            count = int(input("Enter the number of products to add: "))
-            if count >= 0:
-                break
-            else:
-                print("Invalid input. Please enter a positive integer.")
+    #while True:
+            #count = int(input("Enter the number of products to add: "))
+            #if count >= 0:
+             #   break
+            #else:
+             #   print("Invalid input. Please enter a positive integer.")
 
     products = []
-    for i in range(count):
-        name = input(f"Enter the name of product {i+1}: ")
-        price = float(input(f"Enter the price of product {i+1}: "))
-        product = createProduct(name, price)
-        products.append(product)
+    #for i in range(count):
+        #name = input(f"Enter the name of product {i+1}: ")
+        #price = float(input(f"Enter the price of product {i+1}: "))
+     #   product = createProduct(name, price)
+      #  products.append(product)
     
     calculateTotalPrice(products)
     
