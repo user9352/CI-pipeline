5436d1d48997030ad415b424f899b628ffd46935
Test DP <osima1@student.euba.sk>
2023-04-22 14:09:59 +0200

    .

--- src/src.py
+++ src/src.py
@@ -12,23 +12,18 @@ def calculateTotalPrice(products):
         totalPrice += product["price"]
     return totalPrice
 
-def askForProduct():
-    while True:
-        count = int(input("Enter the number of products to add: "))
-        if count >= 0:
-            break
-        else:
-            print("Invalid input. Please enter a positive integer.")
-
+def askForProduct(count, names, prices):
     products = []
     for i in range(count):
-        name = input(f"Enter the name of product {i+1}: ")
-        price = float(input(f"Enter the price of product {i+1}: "))
+        name = names[i]
+        price = prices[i]
         product = createProduct(name, price)
         products.append(product)
     
-    calculateTotalPrice(products)
-    
+    return calculateTotalPrice(products)
 
-askForProduct()
-print('Total Price:', totalPrice)
+#count = 2
+#names = ["Product 1", "Product 2"]
+#prices = [6, 3.51]
+total_price = askForProduct(count, names, prices)
+print('Total Price:', total_price)
--- tests/unit_test.py
+++ tests/unit_test.py
@@ -4,19 +4,12 @@ sys.path.append('/home/runner/work/CI-pipeline/CI-pipeline/src')
 
 from src import *
 
-def test_askForProduct(mocker, capsys, monkeypatch):
-    # Mock user input
-    user_input = "2\nProduct 1\n6\nProduct 2\n3.51\n"
-    monkeypatch.setattr('builtins.input', lambda _: user_input)
-
-    # Call the function being tested
-    askForProduct()
-
-    # Capture the printed output
-    captured = capsys.readouterr()
-
-    # Assert the expected output
-    assert captured.out == "Total Price: 9.51\n"
+def test_askForProduct():
+    count = 2
+    names = ["Product 1", "Product 2"]
+    prices = [6, 3.51]
+    total_price = askForProduct(count, names, prices)
+    assert total_price == 9.51, "Total price is not calculated correctly"
 
 def test_createProduct():
     product = createProduct("Test Product", 5.5)
