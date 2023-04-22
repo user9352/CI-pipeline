4cf6010ba786c95504024da46ebc688ec79134c4
Test DP <osima1@student.euba.sk>
2023-04-22 14:22:28 +0200

    d

--- tests/unit_test.py
+++ tests/unit_test.py
@@ -4,13 +4,6 @@ sys.path.append('/home/runner/work/CI-pipeline/CI-pipeline/src')
 
 from src import *
 
-def test_askForProduct():
-    count = 2
-    names = ["Product 1", "Product 2"]
-    prices = [6, 3.51]
-    total_price = askForProduct(count, names, prices)
-    assert total_price == 9.51, "Total price is not calculated correctly"
-
 def test_createProduct():
     product = createProduct("Test Product", 3.51)
     assert product == {"name": "Test Product", "price": 3.51}
@@ -19,3 +12,10 @@ def test_calculateTotalPrice():
     products = [{"name": "Product 1", "price": 6}, {"name": "Product 2", "price": 9.51}]
     total = calculateTotalPrice(products)
     assert total == 9.51
+
+def test_askForProduct():
+    count = 2
+    names = ["Product 1", "Product 2"]
+    prices = [6, 3.51]
+    total_price = askForProduct(count, names, prices)
+    assert total_price == 9.51, "Total price is not calculated correctly"
